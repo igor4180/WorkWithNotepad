d2ce4aa62f56c4406273837202f98c45dd9f2461
--- <---.mainry>
2023-02-10 20:43:04 +0300

    Добавьте файлы проекта.

---
+++ WorkWithNotepad/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace WorkWithNotepad
+{
+	internal class Program
+	{
+		[DllImport("user32.dll")]
+		private static extern int SendMessage(IntPtr windowHandle, uint message, string addString1, string addString2);
+		static void Main(string[] args)
+		{
+			Process[] processes = Process.GetProcesses();
+			Process notepad = new Process();
+			foreach (Process process in processes)
+			{
+				if (process.ProcessName == "notepad")
+				{
+					notepad = process; break;
+				}
+			}
+			System.Timers.Timer timer = new System.Timers.Timer();
+			timer.Interval = 1000;
+			timer.Elapsed += UpdateTime;
+		}
+
+		public static void UpdateTime(Process process)
+		{
+			SendMessage(process.MainWindowHandle, 0x0C, string.Empty, DateTime.Now.ToString());
+		}
+	}
+}
